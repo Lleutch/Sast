47916964113df246eb3406eb6fdfff48171b7742
Fahd <fa1915@ic.ac.uk>
2016-07-05 21:22:09 +0200

    Defined functions for creating types + Generate a simple type provider

--- GenerativeTypeProviderExample/Library1.fs
+++ GenerativeTypeProviderExample/Library1.fs
@@ -1,4 +1,4 @@
-﻿module GenerativeTypeProviderExample.Provider
+﻿namespace GenerativeTypeProviderExample
 
 open FSharp.Core.CompilerServices
 
@@ -11,43 +11,29 @@ open System
 open System.Net.Sockets
 open System.IO
 open System.Threading
-
+open GenerativeTypeProviderExample.TypeGeneration
 
 [<TypeProvider>]
-type ProviderTest(config : TypeProviderConfig) as this =
+type GenerativeTypeProvider(config : TypeProviderConfig) as this =
     inherit TypeProviderForNamespaces ()
+   
+    let tmpAsm = Assembly.LoadFrom(config.RuntimeAssembly)
     
-    let internal ns = "GenerativeTypeProviderExample.Provided"
-    let provAsm = ProvidedAssembly(Path.ChangeExtension(Path.GetTempFileName(), ".dll"))
-    
+    let providedType = TypeGeneration.createProvidedType tmpAsm "TypeProvider"
+
     let createType (name:string) (parameters:obj[]) =
-        
-        
-        let ctor = ProvidedConstructor([], InvokeCode = fun args -> <@@ "hey " + string n + " nombre de receiver" + string numberOfRoles @@> )
-        let myMethod = ProvidedMethod("Start",[], firstStateType,InvokeCode = (fun args -> let c = firstStateType.GetConstructors().[0] 
-                                                                                           agentRouter.Start()
-                                                                                           Expr.NewObject(c, [])))  
-        let t = ProvidedTypeDefinition(asm,ns,name,Some typeof<obj>)
-        let memberList = listTypes |> List.append list2 |> List.append list1 
-        t.AddMembers(memberList)
-        t.AddMember(myMethod)
-        t.AddMember(ctor)
-        t
+        match parameters with
+            |[||] -> name
+                        |> TypeGeneration.createProvidedType tmpAsm
+                        |> TypeGeneration.addMember (TypeGeneration.createMethodType "You" [])
+                        |> TypeGeneration.addMember (TypeGeneration.createProvidedIncludedType "NestedType" |> (TypeGeneration.createMethodType "NestedMethod" [] |> TypeGeneration.addMember ))
+                        |> TypeGeneration.addTypeToProvidedType
+            | _ -> failwith "Shouldn't be any parameters"
 
+    let parameters = []
 
-    // let assembly = ProvidedAssembly(Path.ChangeExtension(Path.GetTempFileName(), ".dll"))
-    // let c = assembly.AddTypes
-
-    let providedType = ProvidedTypeDefinition(asm,ns,"RealProvider",Some typeof<obj>)
-    let parameters = [ProvidedStaticParameter("Protocol",typeof<string>);
-                      ProvidedStaticParameter("Local",typeof<bool>);
-                      ProvidedStaticParameter("Senders",typeof<Map<string,string*int>>,parameterDefaultValue = Map.empty<string,string*int> );
-                      ProvidedStaticParameter("Receiver",typeof<string*int>,parameterDefaultValue= ("127.0.0.1",5000) )]
-
-    // ProvidedStaticParameter("SampleIsList", typeof<bool>, parameterDefaultValue = false) INSTEAD OF OPTIONSSSSSS 
-
-    do
+    do 
         providedType.DefineStaticParameters(parameters,createType)
-        this.AddNamespace(ns, [providedType] )
+        this.AddNamespace(ns, [TypeGeneration.addTypeToProvidedType providedType] )
 [<assembly:TypeProviderAssembly>]
     do()
--- GenerativeTypeProviderExample/TypeGeneration.fs
+++ GenerativeTypeProviderExample/TypeGeneration.fs
@@ -5,17 +5,32 @@ open FSharp.Core.CompilerServices
 open Microsoft.FSharp.Quotations
 open ProviderImplementation.ProvidedTypes // open the providedtypes.fs file
 open System.Reflection // necessary if we want to use the f# assembly
-
-open Microsoft.FSharp.Data
-open System
-open System.Net.Sockets
+    
 open System.IO
-open System.Threading
 
 
 let internal ns = "GenerativeTypeProviderExample.Provided"
 let asm = ProvidedAssembly(Path.ChangeExtension(Path.GetTempFileName(), ".dll"))
 
+let baseType = typeof<obj>
 
-let internal createProvidedType () =
-    
+let internal createProvidedType assembly name = 
+    ProvidedTypeDefinition(assembly, ns, name, Some baseType, IsErased=false)
+
+let internal createProvidedIncludedType name = 
+    ProvidedTypeDefinition(name,Some baseType, IsErased=false)
+
+let internal createMethodType name param=
+    ProvidedMethod( name, param, baseType, InvokeCode = (fun args -> <@@ "Hello" + name @@> ))
+
+let internal addTypeToProvidedType (providedType:ProvidedTypeDefinition)=
+    asm.AddTypes([providedType])
+    providedType
+
+let addMember memberToAdd (providedType:ProvidedTypeDefinition) =
+    providedType.AddMember(memberToAdd)
+    providedType
+
+let internal addMethod methodType (providedType:ProvidedTypeDefinition) = 
+    providedType.AddMember methodType
+    providedType    
