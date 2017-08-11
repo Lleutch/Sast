ea2cc6ce886d19144dc4a8aec7905b6cc40ce45e
Ray Hu <raymond.hu@imperial.ac.uk>
2017-08-11 17:52:09 +0100

    RH windows setup.

--- Examples/Fibonacci/Fibonacci.fsx
+++ Examples/Fibonacci/Fibonacci.fsx
@@ -13,17 +13,15 @@ let delims = """ [ {"label" : "ADD", "delims": {"delim1": [":"] , "delim2": [","
 let typeAliasing =
     """ [ {"alias" : "Int", "type": "System.Int32"} ] """
 
-
-C:\Users\rn710\Repositories\GenerativeTypeProviderExample\Examples\Fibonacci
-
+// C:/cygwin64/home/rhu/code/vs/scribble/github.com/rumineykova/Sast/Examples/Fibonacci/
 type Fib = 
-    Provided.TypeProviderFile<"C:/Users/rn710/Repositories/GenerativeTypeProviderExample/Examples/Fibonacci/scribble-java/Fib.scr" // Fully specified path to the scribble file
+    Provided.TypeProviderFile<"../../../Examples/Fibonacci/FibFsm.txt" // Fully specified path to the scribble file
                                ,"Adder" // name of the protocol
                                ,"C" // local role
                                ,"../../../Examples/Fibonacci/config.yaml" // config file containing IP and port for each role and the path to the scribble script
                                ,Delimiter=delims 
                                ,TypeAliasing=typeAliasing // give mapping from scribble base files to F# types
-                               ,ScribbleSource = ScribbleSource.LocalExecutable> // choose one of the following options: (LocalExecutable | WebAPI | File)
+                               ,ScribbleSource = ScribbleSource.File> // choose one of the following options: (LocalExecutable | WebAPI | File)
 
 let numIter = 10-2
 let S = Fib.S.instance
