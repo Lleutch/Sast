f9efe27950b805f34e96a86c348205ea4cc06931
Rumyana Neykova <rumyana.neykova10@imperial.ac.uk>
2017-08-13 17:55:48 +0100

    Fix ExecuteLocal option to work on Windows 10

--- Examples/Fibonacci/Fibonacci.fsx
+++ Examples/Fibonacci/Fibonacci.fsx
@@ -15,14 +15,14 @@ let typeAliasing =
 
 // C:/cygwin64/home/rhu/code/vs/scribble/github.com/rumineykova/Sast/Examples/Fibonacci/
 type Fib = 
-    Provided.TypeProviderFile<"../../../Examples/Fibonacci/Fib.scr" // Fully specified path to the scribble file
+    Provided.TypeProviderFile<"C:/Users/rn710/Repositories/GenerativeTypeProviderExample/Examples/Fibonacci/Fib.scr" // Fully specified path to the scribble file
                                ,"Adder" // name of the protocol
                                ,"C" // local role
                                ,"../../../Examples/Fibonacci/config.yaml" // config file containing IP and port for each role and the path to the scribble script
                                ,Delimiter=delims 
                                ,TypeAliasing=typeAliasing // give mapping from scribble base files to F# types
-                               ,ScribbleSource = ScribbleSource.LocalExecutable> // choose one of the following options: (LocalExecutable | WebAPI | File)
-
+                               ,ScribbleSource = ScribbleSource.LocalExecutable // choose one of the following options: (LocalExecutable | WebAPI | File)
+                              >
 let numIter = 10-2
 let S = Fib.S.instance
 
