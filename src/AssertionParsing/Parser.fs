dd0a7a720464d87ac5a2febd1fb80268bae1484a
Rumyana Neykova <rumyana.neykova10@imperial.ac.uk>
2017-08-23 00:54:13 +0100

    Add a project for parsing expressions

---
+++ src/AssertionParsing/Parser.fs
@@ -0,0 +1,143 @@
+﻿//#r "./packages/FSharp.Data/lib/net45/FSharp.Data.dll"
+//#r "./packages/FParsec/lib/net45-client/FParsecCS.dll"
+//#r "./packages/FParsec/lib/net45-client/FParsec.dll"
+
+namespace AssertionParsing
+
+open System.Text.RegularExpressions
+open FParsec
+
+//===============
+
+type Op =
+        | Minus | Plus | Subtract | Multiply 
+        | LT | GT | Eq | NotEq | LTEq | GTEq | AndOp | OrOp
+        with 
+            override x.ToString() =
+                match x with
+                | Minus -> "-"
+                | Plus -> "+"
+                | Subtract -> "-"
+                | Multiply -> "*"
+                | LT -> "<"
+                | GT -> ">"
+                | Eq -> "="
+                | NotEq -> "<>"
+                | LTEq -> "<="
+                | GTEq -> ">="  
+                | AndOp -> "&&"
+                | OrOp -> "||"
+
+type identifier = string 
+         
+type Value =
+        // Primitives
+        | Bool  of bool   (* true | false *)
+        | IntC of System.Int32 
+        | Ident of identifier
+
+type Expr =
+
+        // Primitives
+        | Literal of Value
+        | Ident  of identifier (* ident *)
+
+        | Args of Expr list
+        | Arithmetic of Expr * Op * Expr
+        | Comparison of Expr * Op * Expr
+        | Logical    of Expr * Op * Expr
+        | UnaryOp    of Op * Expr
+        
+        // Logical
+        | Not        of Expr
+        | BinLogical of Expr * Op * Expr
+        | And        of Expr
+        | Or        of Expr
+
+
+module AssertionParser = 
+    let ws = spaces
+    let str = pstring
+    let all, allRef = createParserForwardedToRef()
+    
+    let specialChars = ['+'; '-'; '*'; '(';')';'>';'<';'='; '&';'|';' ']
+    
+    let number c = isDigit c 
+    let pConst: Parser<_, unit> = pint32 .>> ws |>> (fun x -> Literal(IntC(x)))
+    let pVarName:Parser<_, unit> = (manyChars (noneOf specialChars)) |>> Ident
+    let xbool =     (stringCIReturn "true" (Bool true)   |>> Literal)
+                    <|> (stringCIReturn "false" (Bool false) |>> Literal)
+
+
+    let xprim = pConst <|> xbool <|> pVarName  
+
+    /// Configure the operator precedence parser to handle complex expressions
+    let oppa = new OperatorPrecedenceParser<Expr, unit, unit>()
+
+    let parithmetic = oppa.ExpressionParser
+
+    oppa.TermParser <- (xprim .>> ws) 
+    //<|> logicals .>> ws <|> between (str "(" .>> ws) (str ")" .>> ws) parithmetic
+
+    type Assoc = Associativity
+    
+    /// Binary Operators
+    oppa.AddOperator(InfixOperator("+", ws, 1, Assoc.Left,  fun x y -> Arithmetic(x, Plus, y)))
+    oppa.AddOperator(InfixOperator("-", ws, 1, Assoc.Left,  fun x y -> Arithmetic(x, Minus, y)))    
+    oppa.AddOperator(InfixOperator("*", ws, 2, Assoc.Left,  fun x y -> Arithmetic(x, Multiply, y)))
+
+
+    /// Unary Operators
+    oppa.AddOperator(PrefixOperator("-", ws, 6, true, fun x -> UnaryOp(Minus, x)))
+    // Essentially a no-op
+    oppa.AddOperator(PrefixOperator("+", ws, 6, true, fun x -> x))
+
+    /// Logical Binary Operators
+    let oppc = new OperatorPrecedenceParser<Expr, unit, unit>()
+    let pcomparison = oppc.ExpressionParser
+    let termc = (parithmetic .>> ws) //<|> between (str "(" .>> ws) (str ")" .>> ws) parithmetic
+    oppc.TermParser <- termc
+
+    oppc.AddOperator(InfixOperator("=",  ws, 1, Assoc.Left, fun x y -> Comparison(x, Eq,    y)))
+    oppc.AddOperator(InfixOperator("<>", ws, 1, Assoc.Left, fun x y -> Comparison(x, NotEq, y)))
+    oppc.AddOperator(InfixOperator("!=", ws, 1, Assoc.Left, fun x y -> Comparison(x, NotEq, y)))
+    oppc.AddOperator(InfixOperator(">",  ws, 1, Assoc.Left, fun x y -> Comparison(x, GT,    y)))
+    oppc.AddOperator(InfixOperator("<",  ws, 1, Assoc.Left, fun x y -> Comparison(x, LT,    y)))
+    oppc.AddOperator(InfixOperator(">=", ws, 1, Assoc.Left, fun x y -> Comparison(x, GTEq,  y)))
+    oppc.AddOperator(InfixOperator("<=", ws, 1, Assoc.Left, fun x y -> Comparison(x, LTEq,  y)))
+
+
+    let oppb = new OperatorPrecedenceParser<Expr, unit, unit>()
+    let bcomparison = oppb.ExpressionParser
+    let termb = between (str "(" .>> ws) (str ")" .>> ws) bcomparison <|>  (pcomparison .>> ws) 
+    oppb.TermParser <- termb
+
+    oppb.AddOperator(InfixOperator("&&",  ws, 1, Assoc.Left, fun x y -> BinLogical(x, AndOp, y)))
+    oppb.AddOperator(InfixOperator("||", ws, 1, Assoc.Left, fun x y -> BinLogical(x, OrOp , y)))
+
+
+    // do exprRef := opp.ExpressionParser
+    allRef := oppb.ExpressionParser
+
+
+        // The full parser, terminating with an EOF marker
+    let xparser = ws >>. all .>> ws .>> eof
+    
+    let parse expr = 
+        match (run xparser expr) with 
+            | Failure (error,_,_) -> 
+                printfn "%s" error
+                None
+            | Success (res,_,_) -> Some res
+
+    let testExprAsStr() = 
+        run xparser "x+1 < y+2 || x+1" |> ignore
+        run xparser "x+1 <5 && x<5   && x=y" |> ignore
+        run xparser "(x+1 <5 && x<5   && x=y)" |> ignore
+        run xparser " x<3 && y<6 " |> ignore
+        run xparser " x+1 < y*3 "|> ignore
+        run xparser "x+1 > 4" |> ignore
+        run xparser "x=y" |> ignore
+        run xparser "x<y && x>10" |> ignore
+        run xparser "x<y" |> ignore
+
